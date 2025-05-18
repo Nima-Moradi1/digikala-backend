37595ce12fdd82241227c18d135a0c65ed54751a
nima moradi <mnima8100@gmail.com>
2025-05-18 23:35:45 +0330

    constants

---
+++ common/constant/order.const.js
@@ -0,0 +1,13 @@
+const OrderStatus = {
+  Pending: "pending",
+  Ordered: "ordered",
+  InProcess: "in-process",
+  Packing: "packing",
+  InTransit: "in-transit",
+  Canceled: "canceled",
+  Delivered: "delivered",
+};
+
+module.exports = {
+  OrderStatus,
+};
